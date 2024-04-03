ecd5dfa06ad396922a5f00257a2712ecd7a47a20
Fiona <fiona_weiyuan@hotmail.com>
2024-04-03 10:12:52 +0800

    Add comparison component

---
+++ src/app/features/comparison/comparison.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+//import * as Diff from 'diff';
+
+
+@Component({
+  selector: 'app-comparison',
+  templateUrl: './comparison.component.html',
+  styleUrl: './comparison.component.css'
+})
+export class ComparisonComponent implements OnInit{
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+}
